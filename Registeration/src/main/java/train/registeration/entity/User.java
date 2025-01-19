f7c2e23b5e55915988f706c88cc19b1ee9b06f3b
RezaRfahi <refahireza82@gmail.com>
2025-01-20 01:10:02 +0330

    User Entity initial config
    configuration without any relationships

--- Registeration/src/main/java/train/registeration/entity/User.java
+++ Registeration/src/main/java/train/registeration/entity/User.java
@@ -1,8 +1,11 @@
 package train.registeration.entity;
 
 import jakarta.persistence.*;
+import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Date;
 
 @Getter
 @Setter
@@ -14,4 +17,19 @@ public class User {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(unique = true, nullable = false, length = 50)
+    private String username;
+
+    @Column()
+    private String password;
+
+    @Column(unique = true, length = 50)
+    private String email;
+
+    @Column(unique = true, length = 13)
+    private String phone;
+
+    private Date birthday;
+
+    private Date registrationDate;
 }
