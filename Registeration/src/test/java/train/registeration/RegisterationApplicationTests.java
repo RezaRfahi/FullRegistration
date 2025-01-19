b0ed903f274bf918b742657050ec8fd61167fd6c
RezaRfahi <refahireza82@gmail.com>
2025-01-20 00:02:42 +0330

    Initial Commit
    Setup Project with its dependencies

---
+++ Registeration/src/test/java/train/registeration/RegisterationApplicationTests.java
@@ -0,0 +1,13 @@
+package train.registeration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
